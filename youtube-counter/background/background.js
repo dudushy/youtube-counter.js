94e9c6982428037eaa82005ec5e1b99a8d8db32c
dudushy <eduardotalarico1337@gmail.com>
2022-06-03 17:51:30 -0300

    Contando URL (click)

--- youtube-counter/background/background.js
+++ youtube-counter/background/background.js
@@ -1,6 +1,15 @@
 let color = '#3aa757';
 
+// let watchedVideos = {
+//     "test": 1,
+//     "crrUrl": ""
+// }
+
+let watchedVideos = {}
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+
+    chrome.storage.sync.set({ watchedVideos });
 });
--- youtube-counter/popup/popup.js
+++ youtube-counter/popup/popup.js
@@ -2,23 +2,45 @@
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+    changeColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+
+    chrome.storage.sync.get("watchedVideos", ({ watchedVideos }) => {
+        console.log("before", watchedVideos);
+        // console.log(window.location.toString());
+
+        // watchedVideos['crrUrl'] = window.location.toString();
+        // watchedVideos['test'] = watchedVideos['test'] + 1;
+
+        const url = window.location.toString();
+
+        if (watchedVideos[url]) {
+            watchedVideos[url] = watchedVideos[url] + 1;
+        } else {
+            Object.assign(watchedVideos, {
+                [url]: 1
+            });
+        }
+
+        console.log("after", watchedVideos);
+
+        chrome.storage.sync.set({ watchedVideos });
+    });
 }
